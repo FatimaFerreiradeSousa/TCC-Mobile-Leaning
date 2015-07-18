aec51695331700e4d349068bb77f277f3407c510
Fatinha <fatinha.sg@hotmail.com>
2015-07-17 21:26:07 -0300

    Resolucao de testes I

--- Mobile-Edu/src/main/java/com/br/controladores/ControladorTeste.java
+++ Mobile-Edu/src/main/java/com/br/controladores/ControladorTeste.java
@@ -1,12 +1,11 @@
 package com.br.controladores;
 
-import com.br.entidades.Pergunta;
-import com.br.entidades.Teste;
-import com.br.entidades.Topico;
+import com.br.entidades.*;
 import com.br.fachada.Fachada;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 
@@ -21,9 +20,11 @@ public class ControladorTeste implements Serializable {
     @EJB
     private Fachada fachada; 
     private Teste teste;
+    private List<String> respostas;
     
     public ControladorTeste() {
         teste = new Teste();
+        respostas = new ArrayList();
     }
 
     public Teste getTeste() {
@@ -34,8 +35,24 @@ public class ControladorTeste implements Serializable {
         this.teste = teste;
     }
 
+    public List<String> getRespostas() {
+        return respostas;
+    }
+
+    public void setRespostas(List<String> respostas) {
+        this.respostas = respostas;
+    }
+
     public String paginaVisualizarTeste(Topico topico){
         teste = fachada.buscarExercicio(topico.getCodigoTeste());
+        return "pagina-responder-teste?faces-redirect=true";
+    }
+    
+    public String enviarRespostas(){
+        System.out.println("Aqui!");
+        for(String resposta: respostas){
+            System.out.println("Numero: " +resposta);
+        }
         
         return "pagina-responder-teste?faces-redirect=true";
     }
