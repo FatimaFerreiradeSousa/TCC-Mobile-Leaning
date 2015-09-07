38ba1670530e2fc27e26a89ce0f8a1ac0d9e6d95
Fatinha de Sousa <fatinha.sg@hotmail.com>
2015-09-06 21:16:41 -0300

    Pagina iniicial css

--- Mobile-Edu/src/main/java/com/br/controladores/ControladorLogin.java
+++ Mobile-Edu/src/main/java/com/br/controladores/ControladorLogin.java
@@ -2,55 +2,37 @@ package com.br.controladores;
 
 import com.br.entidades.*;
 import com.br.fachada.Fachada;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Date;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Fatinha de Sousa
  */
-
 @Named(value = "controladorLogin")
 @SessionScoped
-public class ControladorLogin implements Serializable{
+public class ControladorLogin implements Serializable {
 
     @EJB
     Fachada fachada;
-    private Pessoa pessoa;
-    private Aluno aluno;
-    private Professor professor;
     private String usuario;
+    private String login;
+    private String senha;
     
+    HttpSession session;
+
     public ControladorLogin() {
-        pessoa = new Pessoa();
-        aluno = new Aluno();
-        professor = new Professor();
-    }
-
-    public Pessoa getPessoa() {
-        return pessoa;
-    }
-
-    public void setPessoa(Pessoa pessoa) {
-        this.pessoa = pessoa;
-    }
-
-    public Aluno getAluno() {
-        return aluno;
-    }
-
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
-
-    public Professor getProfessor() {
-        return professor;
-    }
-
-    public void setProfessor(Professor professor) {
-        this.professor = professor;
+        login = null;
+        senha = null;
+        usuario = null;
     }
 
     public String getUsuario() {
@@ -60,10 +42,83 @@ public class ControladorLogin implements Serializable{
     public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
-    
-    public String salvarUsuario(){
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String salvarUsuario() throws IOException {
+        System.out.println("Usuario: " + usuario);
+        String caminho
+                = "C:\\Users\\Fatinha de Sousa\\Documents\\Repositorios\\TCC-Mobile-Learning\\Mobile-Edu\\Imagens\\imgPadrao\\perfil.png";
+
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) context.getRequest();
+
+        if (usuario.equalsIgnoreCase("Aluno")) {
+            Aluno aluno = new Aluno();
+            aluno.setLogin(login);
+            aluno.setSenha(senha);
+            aluno.setFoto(caminho);
+            aluno.setDataParticipacao(new Date());
+            fachada.salvarAluno(aluno);
+            login = null;
+            senha = null;
+            context.redirect(request.getContextPath());
+        } else {
+            Professor professor = new Professor();
+            professor.setLogin(login);
+            professor.setSenha(senha);
+            professor.setFoto(caminho);
+            professor.setDataParticipacao(new Date());
+
+            fachada.salvarProfessor(professor);
+            context.redirect(request.getContextPath());
+        }
+
+        return null;
+    }
+
+    public void loginUsuario() throws IOException {
+        
         System.out.println("Usuario: " +usuario);
         
-        return null;
+        if (usuario.equalsIgnoreCase("Aluno")) {
+
+            Aluno aluno = fachada.loginAluno(login, senha);
+
+            if (aluno != null) {
+                String loginPage = "/md-aluno/pageInicialAluno.jsf";
+                ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+                HttpServletRequest request = (HttpServletRequest) context.getRequest();
+                session = (HttpSession) context.getSession(false);
+                context.getSessionMap().put("aluno", aluno);
+                context.redirect(request.getContextPath() + loginPage);
+            }
+        } else {
+            
+            Professor professor = fachada.loginProfessor(login, senha);
+
+            if (professor != null) {
+                String loginPage = "/md-professor/page-inicial-professor.jsf";
+                ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+                HttpServletRequest request = (HttpServletRequest) context.getRequest();
+                session = (HttpSession) context.getSession(false);
+                context.getSessionMap().put("professor", professor);
+                context.redirect(request.getContextPath() + loginPage);
+            }
+        }
     }
 }
